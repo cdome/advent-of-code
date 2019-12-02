fe7e7717b65b9e1e0f5f68414d17f2c25d1fa95f
cdome <1021354+cdome@users.noreply.github.com>
2019-12-01 23:36:26 -0700

    kt day 02 - int computer instrunctions are defined as map of functions

--- 2019/kotlin-check/src/Day02a.kt
+++ 2019/kotlin-check/src/Day02a.kt
@@ -9,13 +9,13 @@ fun runIntCodeV1(memory: List<Int>, noun: Int, verb: Int): Int {
     rwMemory[1] = noun
     rwMemory[2] = verb
 
-    val knownInstructions = listOf(1, 2)
+    val knownInstructions = mapOf<Int, (Int, Int) -> Int>(1 to Int::plus, 2 to Int::times)
     var instructionPointer = 0
-    while (rwMemory[instructionPointer] in knownInstructions) {
-        rwMemory[rwMemory[instructionPointer + 3]] = if (rwMemory[instructionPointer] == 1)
-            rwMemory[rwMemory[instructionPointer + 1]] + rwMemory[rwMemory[instructionPointer + 2]]
-        else
-            rwMemory[rwMemory[instructionPointer + 1]] * rwMemory[rwMemory[instructionPointer + 2]]
+    while (rwMemory[instructionPointer] in knownInstructions.keys) {
+        rwMemory[rwMemory[instructionPointer + 3]] = knownInstructions[rwMemory[instructionPointer]]!!(
+            rwMemory[rwMemory[instructionPointer + 1]],
+            rwMemory[rwMemory[instructionPointer + 2]]
+        )
         instructionPointer += 4
     }
     return if (rwMemory[instructionPointer] == 99) rwMemory[0] else -1
